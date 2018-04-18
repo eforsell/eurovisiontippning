58d02b79b8795fc8adb9643fc4458456b02f6ca5
Eskil Forsell <eskil@spotify.com>
2018-04-18 22:46:46 +0200

    Fix login redirect and authentication bugs

--- tippning/views.py
+++ tippning/views.py
@@ -11,6 +11,17 @@ from tippning.utils import semi_points
 
 def semifinal(request, order):
 
+    entries_bets = None
+    entries = None
+    bets = None
+    has_bets = None
+    points = None
+
+    if request.user.is_authenticated:
+        owner = request.user
+    else:
+        owner = None
+
     semi = (SemiFinal.objects
                      .filter(order=order)
                      .order_by('-start_time')
@@ -21,44 +32,46 @@ def semifinal(request, order):
                         Prefetch(
                             "semientry_set__semibet_set",
                             queryset=SemiBet.objects.filter(
-                                owner=request.user
+                                owner=owner
                                 ),
                             to_attr='bets'
                             ),
                         Prefetch(
                             "semiscore_set",
                             queryset=SemiScore.objects.filter(
-                                owner=request.user
+                                owner=owner
                                 )
                             )
                         ).first())
 
-    entries = list(semi.semientry_set.all())
-    entries.sort(key=lambda x: x.start_order)
+    if request.user.is_authenticated:
 
-    has_bets = False
+        entries = list(semi.semientry_set.all())
+        entries.sort(key=lambda x: x.start_order)
 
-    if request.user.is_authenticated and semi.published:
-        bets = [e.bets[0] for e in entries if len(e.bets) == 1]
+        has_bets = False
 
-        if not (len(bets) == 0 and semi.has_started()):
-            has_bets = True
+        if semi.published:
+            bets = [e.bets[0] for e in entries if len(e.bets) == 1]
 
-            if len(bets) == 0 and not semi.has_started():
-                # Create bets if there are none already:
-                bets = []
-                for entry in entries:
-                    sb, _ = SemiBet.objects.get_or_create(owner=request.user,
-                                                          entry=entry)
+            if not (len(bets) == 0 and semi.has_started()):
+                has_bets = True
 
-                return HttpResponseRedirect("")
+                if len(bets) == 0 and not semi.has_started():
+                    # Create bets if there are none already:
+                    bets = []
+                    for entry in entries:
+                        sb, _ = SemiBet.objects.get_or_create(owner=request.user,
+                                                              entry=entry)
 
-    if not has_bets:
-        bets = [None] * len(entries)
+                    return HttpResponseRedirect("")
 
-    entries_bets = zip(entries, bets)
+        if not has_bets:
+            bets = [None] * len(entries)
 
-    points = semi_points(request.user, semi)
+        entries_bets = zip(entries, bets)
+
+        points = semi_points(request.user, semi)
 
     return render(request, 'semifinal.html', {
         'semi': semi,
@@ -82,6 +95,14 @@ def update_semibet(request):
 
 def final(request):
 
+    entries = None
+    has_bets = None
+
+    if request.user.is_authenticated:
+        owner = request.user
+    else:
+        owner = None
+
     final = (Final.objects
                   .order_by('-start_time')
                   .prefetch_related(
@@ -91,48 +112,49 @@ def final(request):
                      Prefetch(
                          "finalentry_set__finalbet_set",
                          queryset=FinalBet.objects.filter(
-                             owner=request.user
+                             owner=owner
                              ),
                          to_attr='bets'
                          ),
                      Prefetch(
                          "finalscore_set",
                          queryset=FinalScore.objects.filter(
-                             owner=request.user
+                             owner=owner
                              )
                          )
                      ).first())
 
-    entries = list(final.finalentry_set.all())
+    if request.user.is_authenticated:
+        entries = list(final.finalentry_set.all())
 
-    has_bets = False
+        has_bets = False
 
-    if request.user.is_authenticated and final.has_semi_entries:
-        bets = [e.bets[0] for e in entries if len(e.bets) == 1]
+        if final.has_semi_entries:
+            bets = [e.bets[0] for e in entries if len(e.bets) == 1]
 
-        if not (len(bets) == 0 and final.has_started()):
-            has_bets = True
+            if not (len(bets) == 0 and final.has_started()):
+                has_bets = True
 
-            if len(bets) == 0 and not final.has_started():
-                # Create bets if there are none already:
-                bets = []
-                for entry in entries:
-                    sb, _ = FinalBet.objects.get_or_create(
-                                                owner=request.user,
-                                                entry=entry,
-                                                rank=entry.start_order)
+                if len(bets) == 0 and not final.has_started():
+                    # Create bets if there are none already:
+                    bets = []
+                    for entry in entries:
+                        sb, _ = FinalBet.objects.get_or_create(
+                                                    owner=request.user,
+                                                    entry=entry,
+                                                    rank=entry.start_order)
 
-                return HttpResponseRedirect("")
+                    return HttpResponseRedirect("")
 
-    else:
-        bets = [None] * len(entries)
+        else:
+            bets = [None] * len(entries)
 
-    if any(bets):
-        entries.sort(key=lambda x: (x.bets[0].rank is None,
-                                    x.start_order is None,
-                                    x.start_order))
-    else:
-        entries.sort(key=lambda x: (x.start_order is None, x.start_order))
+        if any(bets):
+            entries.sort(key=lambda x: (x.bets[0].rank is None,
+                                        x.start_order is None,
+                                        x.start_order))
+        else:
+            entries.sort(key=lambda x: (x.start_order is None, x.start_order))
 
     return render(request, 'final.html', {
         'final': final,
