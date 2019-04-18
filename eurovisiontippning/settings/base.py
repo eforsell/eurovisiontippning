023faaeb360404868da33567e28d9d0c13d80455
Eskil Forsell <eskil@spotify.com>
2019-04-18 15:02:56 +0200

    Rename static directories

--- eurovisiontippning/settings/base.py
+++ eurovisiontippning/settings/base.py
@@ -154,9 +154,5 @@ COUNTRIES_ONLY = [
     'TR', 'UA'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
