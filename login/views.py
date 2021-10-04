0cc3fcfad562c413e473ddeae01ce06e8b750741
Eskil Forsell <eskil.forsell@gmail.com>
2021-10-05 00:05:32 +0200

    Add user delete button to privacy page

--- login/views.py
+++ login/views.py
@@ -4,20 +4,52 @@ import string
 import base64, hmac, hashlib
 
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth import logout 
 
+from django.contrib.auth.models import User
 from social_django.models import UserSocialAuth
 
 import login.views
 
 
-def confirm_deletion(request):
-    return render(request, 'confirm_deletion.html')
+def confirm_deletion(request, fb=1):
+    _fb = int(fb)
+
+    if _fb == 0:
+        text = 'Din användare all lagrad data har tagits bort. För att anända Eurovisiontippning.se måste du skapa en ny användare genom att logga in med hjälp av antingen Google eller Facebook.'
+    else:
+        text = 'Din koppling till Facebook och all lagrad data har tagits bort. För att anända Eurovisiontippning.se måste du på nytt logga in med hjälp av antingen Google eller Facebook.'
+    return render(request, 'confirm_deletion.html', {
+        'text': text
+    })
+
+
+@login_required
+def delete_user(request):
+  if request.method == 'POST':
+    try:
+        social_auth = UserSocialAuth.objects.get(user=request.user)
+        social_auth.delete()
+    except UserSocialAuth.DoesNotExist:
+        pass
+
+    try:
+        request.user.delete()
+    except:
+        return HttpResponse(status=400, content='Couldn\'t delete user data')
+    
+    logout(request)    
+    return redirect('confirm_deletion', fb=0)
+
+  else:
+    return HttpResponse(status=403, content='Not allowed')
 
 
 @method_decorator(csrf_exempt, name='dispatch')
